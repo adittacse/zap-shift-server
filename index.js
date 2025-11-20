d41fe89234c5a1d38f7799c2a3b97a4e60e697e0
Aditta Chakraborty <cseaditta@gmail.com>
2025-11-21 01:27:29 +0600

    mongodb added and parcels post api created

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const dotenv = require("dotenv");
 dotenv.config();
 const port = process.env.PORT || 3000;
@@ -9,10 +10,50 @@ const port = process.env.PORT || 3000;
 app.use(express());
 app.use(cors());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gkaujxr.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        const db = client.db("zapShiftDB");
+        const parcelsCollection = db.collection("parcels");
+
+        // parcel related api
+        app.get("/parcels", async (req, res) => {
+            //
+        });
+
+        app.post("/parcels", async (req, res) => {
+            const parcel = req.body;
+            const result = await parcelsCollection.insertOne(parcel);
+            res.send(result);
+        });
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!",);
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
     res.send("Zap Shift server is running!");
 });
 
 app.listen(port, () => {
-    console.log(`Zap Shift Server listening on ${process.env.PROTOCOL}://${process.env.HOST}:${process.env.PORT}`);
+    console.log(
+        `Zap Shift Server listening on ${process.env.PROTOCOL}://${process.env.HOST}:${process.env.PORT}`,
+    );
 });
