98d4df07d26a025d4dc1f3e113354798f9de1dc4
Aditta Chakraborty <cseaditta@gmail.com>
2025-12-01 10:52:30 +0600

    rideleted delete api created

--- index.js
+++ index.js
@@ -272,6 +272,13 @@ async function run() {
             res.send(result);
         });
 
+        app.delete("/riders/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await ridersCollection.deleteOne(query);
+            res.send(result);
+        });
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!",);
